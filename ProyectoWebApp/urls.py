58bf7a880de0ac167f61336d68f5b703ee154847
Tomas Estrada <tomasenriquea@hotmail.com>
2020-08-05 05:16:51 +0200

    Inicio de proyecto, creacion de aplicacion, creacion de vistas, creacion de paginas html, creacion de archivo 'urls' en la aplicacion para ordenar las vistas e importarlas al urls principal del proyecto, se registra la aplicacion al archivo 'setting'. Pruebas de funcionamiento OK.

---
+++ ProyectoWebApp/urls.py
@@ -0,0 +1,26 @@
+"""
+Este archivo se crea para poder ordenar las vistas de una aplicacion dentro de un proyecto, de esta forma cuando se
+tenga mas aplicaciones dentro de un proyecto, se creara dentro de cada aplicacion un archivo 'urls' que se enlazara
+con el 'urls' principal del proyecto y aqui solo se tendra que poder las url como se muestran abajo y en el principal
+del proyecto pondria asi:
+                urlpatterns = [
+                    path('admin/', admin.site.urls),
+                    path("ProyectoWebApp/", include("ProyectoWebApp.urls")),
+                ]
+Donde:
+'ProyectoWebApp/'                ==>> Viene a ser el nombre de la aplicacion pero podemos usar el que queramos ya que
+                                      este viene a ser el enlace con el cual se ubicara en la pagina web.
+'include("ProyectoWebApp.urls")' ==>> Este seria una funcion que agregara todas las url de la aplicacion al proyecto
+                                      principal poniendo su nombre y usando la nomenclatura del punto para ubicar las
+                                      url de la aplicacion.
+"""
+from django.urls import path
+from ProyectoWebApp import views
+
+urlpatterns = [
+    path('', views.home, name="Home"),
+    path('servicios/', views.servicios, name="Servicios"),
+    path('tienda/', views.tienda, name="Tienda"),
+    path('block/', views.block, name="Block"),
+    path('contacto/', views.contacto, name="Contacto"),
+]
