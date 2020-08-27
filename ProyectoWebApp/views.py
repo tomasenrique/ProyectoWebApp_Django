7afc790eae63ff6edd968c59bcff6c99c7a74705
Tomas Estrada <tomasenriquea@hotmail.com>
2020-08-27 22:17:05 +0200

    Se configura el template 'servicios.html' para que reciba los datos y las imagenes guardados desde el panel de administracion.

--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import render, HttpResponse
 
+# para importar los datos de las clases que hay en el archivo 'models' de la carpeta de la app de 'servicios'
+from servicios.models import Servicio
+
 
 # Create your views here.
 
@@ -9,7 +12,14 @@ def home(request):
 
 
 def servicios(request):
-    return render(request, "ProyectoWebApp/servicios.html")
+    """ Desde este archivo 'views' hay que importar todos los servicios que se carguen en el panel de administracion y
+    luego decirle que los muestre en el template asignado a cada vista, en este caso 'servicios.html'      """
+
+    servicios = Servicio.objects.all()  # importara todos los objetos que halla en la clase 'Servicios'
+
+    diccionario_servicio = {"servicios": servicios}  # para almacenar los datos a mostrar
+    # devuelve un renderizado del template servicio
+    return render(request, "ProyectoWebApp/servicios.html", diccionario_servicio)
 
 
 def tienda(request):
